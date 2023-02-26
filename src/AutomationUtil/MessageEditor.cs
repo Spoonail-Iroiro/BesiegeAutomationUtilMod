5d9e8e105480f0a85fad94eab1d780fc6efeb475
spoon <ano@example.com>
2023-02-26 18:23:47 +0900

    Add MessageEditor

---
+++ src/AutomationUtil/MessageEditor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutomationUtil
+{
+    public class MessageEditor
+    {
+        private long _maxIndex;
+
+        public MessageEditor(long maxIndex=99)
+        {
+            _maxIndex = maxIndex;
+            
+        }
+
+        public IEnumerable<string> GetNewMessage(IEnumerable<string> messages, int[] indices, long delta)
+        {
+            foreach (var message in messages)
+            {
+                var tu = TextUtil.ExtractDigits(message);
+                var formatString = tu.Item1;
+                var values = tu.Item2;
+                if (values.Length == 0)
+                {
+                    yield return message;
+                }
+                else
+                {
+                    var targetIndices = indices.Select(i => values.Length - 1 - i);
+
+                    // indexに加算・減算をした後、string.Formatのparams object[]にそのまま渡せるようにobject[]にcast
+                    var newValues = values.Select((str, i) => targetIndices.Contains(i) ? AddIndex(Convert.ToInt64(str), delta).ToString() : str).Cast<object>().ToArray();
+                    var newMessage = String.Format(formatString,newValues);
+                    yield return newMessage;
+                }
+            }
+        }
+
+
+        public long AddIndex(long value, long delta)
+        {
+            var rtn = Util.MMod(value + delta, _maxIndex + 1);
+            return rtn;
+
+        }
+    }
+}
---
+++ src/AutomationUtilTest/MessageEditorTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using AutomationUtil;
+
+namespace AutomationUtilTest
+{
+    [TestClass]
+    public class MessageEditorTest
+    {
+        [TestMethod]
+        public void TestGetNewMessage()
+        {
+            var testcases = new[]
+            {
+                Tuple.Create(
+                    new[] { "up1", "up2", "z", "1up3" }, new int[] { 0 }, 1,
+                    new[] {"up2", "up3", "z", "1up4"}
+                ),
+                Tuple.Create(
+                    new[] { "001up2y3", "1up3", "y", "a2" }, new int[] { 1, 0 }, 1,
+                    new[] { "001up3y4", "2up4", "y", "a3" }                
+                ),
+                Tuple.Create(
+                    new[] { "0up0", "1up1", "2up2" }, new int[] { 0 }, -1,
+                    new[] { "0up99", "1up0", "2up1" }
+                ),
+                Tuple.Create(
+                    new[] { "99up0", "98up1", "97up2" }, new int[] { 1 }, 1,
+                    new[] { "0up0", "99up1", "98up2" }
+                )
+            };
+
+            var editor = new MessageEditor();
+
+            foreach (var tc in testcases)
+            {
+                var result = editor.GetNewMessage(tc.Item1, tc.Item2, tc.Item3);
+                CollectionAssert.AreEqual(tc.Item4, result.ToArray());
+            }
+        }
+    }
+}
